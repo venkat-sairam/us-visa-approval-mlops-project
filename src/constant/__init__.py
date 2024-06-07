d43cc279692cf3845e7021c9a06f9ed0422d8c3b
Venkata Sairam Yanamandra <venkatasairam.y@gmail.com>
2024-06-07 14:47:27 +0530

    Added Model Trainer Constants

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -67,3 +67,13 @@ DATA_TRANSFORMATION_PREPROCESSED_DIR_KEY: str = "preprocessed_dir"
 DATA_TRANSFORMATION_PREPROCESSED_OBJECT_FILE_NAME_KEY: str = (
     "preprocessed_object_file_name"
 )
+
+
+# Model Training related variables
+MODEL_TRAINER_ARTIFACT_DIR = "model_trainer"
+MODEL_TRAINER_CONFIG_KEY = "model_trainer_config"
+MODEL_TRAINER_TRAINED_MODEL_DIR_KEY = "trained_model_dir"
+MODEL_TRAINER_TRAINED_MODEL_FILE_NAME_KEY = "model_file_name"
+MODEL_TRAINER_BASE_ACCURACY_KEY = "base_accuracy"
+MODEL_TRAINER_MODEL_CONFIG_DIR_KEY = "model_config_dir"
+MODEL_TRAINER_MODEL_CONFIG_FILE_NAME_KEY = "model_config_file_name"
