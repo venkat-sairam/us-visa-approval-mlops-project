f1c382aa9fc9fb7f36b037e0f63286fd7d8071c7
Venkata Sairam Yanamandra <venkatasairam.y@gmail.com>
2024-06-07 14:47:49 +0530

    Added Model Trainer Entity details

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -31,3 +31,8 @@ DataTransformationConfig = namedtuple(
         "preprocessed_object_file_path",
     ],
 )
+
+ModelTrainerConfig = namedtuple(
+    "ModelTrainerConfig",
+    ["trained_model_file_path", "base_accuracy", "model_config_file_path"],
+)
