7427ebc7043e9e919330a5a276ef150827a76eab
Venkata Sairam Yanamandra <venkatasairam.y@gmail.com>
2024-06-08 16:42:05 +0530

    added decorator to print the start/end of execution

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -1,5 +1,6 @@
 from src.components.data_transformation import DataTransformation
 from src.components.data_validation import DataValidation
+from src.components.model_trainer import ModelTrainer
 from src.configuration import Configuration
 from src.logger import logging
 from src.components.data_ingestion import DataIngestion
@@ -7,11 +8,22 @@ from src.entity.artifact_entity import (
     DataIngestionArtifact,
     DataTransformationArtifact,
     DataValidationArtifact,
+    ModelTrainerArtifact,
 )
 from src.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
 from src.exception import CustomException
-from src.utils import sys
-from src.configuration import Configuration
+
+
+def print_before_execution(func):
+    def wrapper(*args, **kwargs):
+        logging.info(f"{'>>'*12} Executing function: {func.__name__}  {'<<' * 12}")
+        result = func(*args, **kwargs)
+        logging.info(
+            f"{'>>'*12} Finished executing function: {func.__name__}  {'<<' * 12}"
+        )
+        return result
+
+    return wrapper
 
 
 class TrainPipeline:
@@ -22,30 +34,32 @@ class TrainPipeline:
         except Exception as e:
             raise CustomException(e)
 
+    @print_before_execution
     def start_data_ingestion(self) -> DataIngestionArtifact:
         try:
-
-            logging.info("started the data ingestion from training pipeline.....")
+            logging.info("Started the data ingestion from training pipeline...")
             data_ingestion = DataIngestion(self.config.get_data_ingestion_config())
             self.data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-
-            logging.info("data ingestion completed successfully...")
-
+            logging.info("Data ingestion completed successfully...")
+            return self.data_ingestion_artifact
         except Exception as e:
             raise CustomException(e)
 
+    @print_before_execution
     def start_data_validation(self) -> DataValidationArtifact:
         try:
-            logging.info("started the data validation from training pipeline.....")
+            logging.info("Started the data validation from training pipeline...")
             data_validation = DataValidation(
                 data_ingestion_artifact=self.data_ingestion_artifact,
                 data_validation_config=self.config.get_data_validation_config(),
             )
             self.data_validation_artifact = data_validation.initiate_data_validation()
-            logging.info("data validation completed successfully...")
+            logging.info("Data validation completed successfully...")
+            return self.data_validation_artifact
         except Exception as e:
             raise CustomException(e)
 
+    @print_before_execution
     def start_data_transformation(self) -> DataTransformationArtifact:
         try:
             logging.info("Starting data transformation...")
@@ -54,9 +68,27 @@ class TrainPipeline:
                 data_validation_artifact=self.data_validation_artifact,
                 data_transformation_config=self.config.get_data_transformation_config(),
             )
-            data_transformation_artifact = (
+            self.data_transformation_artifact = (
                 data_transformation.initiate_data_transformations()
             )
-            logging.info("Data transformation completed successfully...!")
+            logging.info("Data transformation completed successfully!")
+            return self.data_transformation_artifact
         except Exception as e:
-            raise CustomException(e, sys)
+            raise CustomException(e)
+
+    @print_before_execution
+    def start_model_training(self):
+        try:
+            logging.info("Starting model training...")
+            print(f"{'*' * 25} Data transformation details are: {'*' * 25}")
+            print(self.data_transformation_artifact)
+            model_trainer = ModelTrainer(
+                data_transformation_artifact=self.data_transformation_artifact,
+                model_trainer_config=self.config.get_model_trainer_config(),
+            )
+            logging.info(f"{'>>' * 10} Model trainer details are: {'<<' * 10}")
+            self.model_trainer_artifact = model_trainer._initiate_model_trainer()
+            logging.info("Model training completed...")
+            return self.model_trainer_artifact
+        except Exception as e:
+            raise CustomException(e)
